0d6ded19dde729e644dfc9ed4023f6168998b98a
Ons3x1 <onsnasr31@gmail.com>
2024-11-04 21:39:35 +0100

    Sonar ran tests+ tests package creation

--- src/test/java/tn/esprit/spring/ControlServiceTests/SubscriptionRestControllerTest.java
+++ src/test/java/tn/esprit/spring/ControlServiceTests/SubscriptionRestControllerTest.java
@@ -1,4 +1,4 @@
-package tn.esprit.spring;
+package tn.esprit.spring.ControlServiceTests;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
--- src/test/java/tn/esprit/spring/ControlServiceTests/SubscriptionServiceTest.java
+++ src/test/java/tn/esprit/spring/ControlServiceTests/SubscriptionServiceTest.java
@@ -1,4 +1,4 @@
-package tn.esprit.spring;
+package tn.esprit.spring.ControlServiceTests;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
