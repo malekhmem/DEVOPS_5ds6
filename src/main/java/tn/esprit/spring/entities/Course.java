c6c066648bb3e4f388bef6df593de32c47ad6eb4
Malek Hmem <malek.hmem@esprit.tn>
2024-11-05 13:56:48 +0100

    gitt1

--- src/main/java/tn/esprit/spring/entities/Course.java
+++ src/main/java/tn/esprit/spring/entities/Course.java
@@ -20,6 +20,7 @@ import lombok.experimental.FieldDefaults;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 @Entity
 public class Course implements Serializable {
+
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
